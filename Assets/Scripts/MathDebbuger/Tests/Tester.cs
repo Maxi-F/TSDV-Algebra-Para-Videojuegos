3e48f4c347041c966bc4eff60a874b6872d5705f
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-05-03 19:57:01 -0300

    Add Ejercicios.dll & start excercise one

--- Assets/Scripts/MathDebbuger/Tests/Tester.cs
+++ Assets/Scripts/MathDebbuger/Tests/Tester.cs
@@ -1,49 +1,70 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MathDebbuger;
 using CustomMath;
+
+enum Excercise
+{
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN
+}
+
 public class Tester : MonoBehaviour
 {
+    [SerializeField] private Excercise excerciseNumber;
+    [SerializeField] private Vector3 aVectorValues;
+    [SerializeField] private Vector3 anotherVectorsValues;
+
+    private Vec3 _vector;
+    private Vec3 _anotherVector;
+    
+    private const string VECTOR_IDENTIFIER = "vector1";
+    private const string ANOTHER_VECTOR_IDENTIFIER = "vector2";
+    private const string RESULT_IDENTIFIER = "result";
+    
     void Start()
     {
-        List<Vector3> vectors = new List<Vector3>();
-        vectors.Add(new Vec3(10.0f, 0.0f, 0.0f));
-        vectors.Add(new Vec3(10.0f, 10.0f, 0.0f));
-        vectors.Add(new Vec3(20.0f, 10.0f, 0.0f));
-        vectors.Add(new Vec3(20.0f, 20.0f, 0.0f));
-        Vector3Debugger.AddVectorsSecuence(vectors, false, Color.red, "secuencia");
-        Vector3Debugger.EnableEditorView("secuencia");
-        Vec3 azul = new Vec3(10, 10, 0);
-        Vec3 verde = new Vec3(0, -7, 0);
-
-        Vector3Debugger.AddVector(new Vec3(10, 10, 0), Color.blue, "elAzul");
-        Vector3Debugger.EnableEditorView("elAzul");
-        Vector3Debugger.AddVector(new Vec3(0, -7, 0), Color.green, "elVerde");
-        Vector3Debugger.EnableEditorView("elVerde");
-
-        Vector3Debugger.AddVector(Vec3.Cross(azul, verde), Color.black, "elNegro");
-        Vector3Debugger.EnableEditorView("elNegro");
-
+        AssignVectors();
+        ExcerciseOne();
     }
-
+    
     // Update is called once per frame
-    void Update()
+    private void OnValidate()
     {
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-        }
-
-        if (Input.GetKeyDown(KeyCode.O))
-        {
-            Vector3Debugger.TurnOffVector("elAzul");
-        }
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            Vector3Debugger.TurnOnVector("elAzul");
-        }
+        Vector3Debugger.DeleteVector(VECTOR_IDENTIFIER);
+        Vector3Debugger.DeleteVector(ANOTHER_VECTOR_IDENTIFIER);
+        Vector3Debugger.DeleteVector(RESULT_IDENTIFIER);
+        
+        AssignVectors();
+        ExcerciseOne();
     }
 
+    private void AssignVectors()
+    {
+        _vector = new Vec3(aVectorValues.x, aVectorValues.y, aVectorValues.z);
+        _anotherVector = new Vec3(anotherVectorsValues.x, anotherVectorsValues.y, anotherVectorsValues.z);
+    }
+
+    private void ExcerciseOne()
+    {
+        Vec3 sumOfVectors = _vector + _anotherVector;
+        
+        Vector3Debugger.AddVector(_vector, Color.green, VECTOR_IDENTIFIER);
+        Vector3Debugger.AddVector(_vector, Color.magenta, ANOTHER_VECTOR_IDENTIFIER);
+        Vector3Debugger.AddVector(sumOfVectors, Color.white, RESULT_IDENTIFIER);
+        Vector3Debugger.EnableEditorView();
+    }
+    
     IEnumerator UpdateVector()
     {
         for (int i = 0; i < 100; i++)
