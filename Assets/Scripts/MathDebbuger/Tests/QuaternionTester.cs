bc0a4b6edb78e228ba57bc994c3ef73390e65657
Maxi-F <maximiliano.a.feldman@gmail.com>
2024-07-05 01:44:45 -0300

    Add Excercise 3

--- Assets/Scripts/MathDebbuger/Tests/QuaternionTester.cs
+++ Assets/Scripts/MathDebbuger/Tests/QuaternionTester.cs
@@ -20,6 +20,9 @@ namespace MathDebbuger.Tests
 
         private Vec3 _excercise2Vec1 = new Vec3(0, 10, 0);
         private Vec3 _excercise2Vec2 = new Vec3(10, 0, 0);
+
+        private Vec3 _excercise3Vec1 = new Vec3(0, 10, 0);
+        private Vec3 _excercise3Vec2 = new Vec3(10, 0, 0);
         
         private Vec3 _vector;
         private const string VectorIdentifier = "vector1";
@@ -49,6 +52,13 @@ namespace MathDebbuger.Tests
                     _vector = rotation2 * _vector;
                     _excercise2Vec2 = rotation2 * _vector;
                     break;
+                case QuaternionExcercise.Three:
+                    MyQuaternion rotation3 =
+                        MyQuaternion.AngleAxis(angle, new Vec3(0.5f, 0.5f, 0));
+
+                    _vector = rotation3 * _vector;
+                    _excercise3Vec2 = MyQuaternion.Inverse(rotation3) * _excercise3Vec2;
+                    break;
             }
         }
 
@@ -72,6 +82,21 @@ namespace MathDebbuger.Tests
                     
                     Gizmos.color = Color.red;
                     Gizmos.DrawLine(transform.position + _vector + _excercise2Vec1, _vector + _excercise2Vec1 + _excercise2Vec2);
+                    break;
+                case QuaternionExcercise.Three:
+                    Vec3 fixedSumX = new Vec3(10, 0, 0);
+                    Vec3 fixedSumY = new Vec3(0, 10, 0);
+                    
+                    Gizmos.DrawLine(transform.position, _vector);
+                    
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawLine(transform.position + _vector, fixedSumX + _excercise3Vec1);
+                    
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawLine(transform.position + fixedSumX + _excercise3Vec1, fixedSumX + _excercise3Vec1 + _excercise3Vec2);
+                    
+                    Gizmos.color = Color.black;
+                    Gizmos.DrawLine(transform.position + fixedSumX + _excercise3Vec1 + _excercise3Vec2, fixedSumX + fixedSumX + fixedSumY + fixedSumY);
                     break;
             }
         }
